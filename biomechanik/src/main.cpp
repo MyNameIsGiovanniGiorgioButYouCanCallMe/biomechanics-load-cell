45bde06e00b5a422f1b267c9be83df11e65752af
Felix Muenzer <felix.muenzer@gmx.de>
2024-11-22 14:47:12 +0100

    [feat] first program

--- biomechanik/src/main.cpp
+++ biomechanik/src/main.cpp
@@ -1,18 +1,41 @@
 #include <Arduino.h>
+#include <HX711.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Pin-Konfiguration
+#define DOUT_PIN A4  // DOUT-Pin des HX711
+#define SCK_PIN A6   // PD_SCK-Pin des HX711
+
+// HX711-Instanz erstellen
+HX711 scale;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    // Seriellen Monitor starten
+    Serial.begin(115200);
+    delay(500);
+
+    // HX711 initialisieren
+    scale.begin(DOUT_PIN, SCK_PIN);
+
+    // Optional: Verstärkung einstellen (Standard: 128)
+    scale.set_gain(128);
+
+    // Kalibrierungswert setzen (Wert anpassen, um das richtige Gewicht zu erhalten)
+    scale.set_scale(2280.f); // todo: Beispiel-Kalibrierwert evtl noch Kalibrieren
+    scale.tare();            // Tare: Gewicht auf Null setzen
+
+    Serial.println("HX711 ist bereit!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
+    // Gewicht in Einheiten auslesen
+    if (scale.is_ready()) {
+        float gewicht = scale.get_units(10); // Durchschnitt von 10 Messungen
+        Serial.print("Gewicht: ");
+        Serial.print(gewicht);
+        Serial.println(" kg");
+    } else {
+        Serial.println("HX711 nicht bereit!");
+    }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    delay(500);
 }
